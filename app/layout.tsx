027778954f8e61574e73110140176c3e1728a535
EnderMRG <moharnab.gogoi@gmail.com>
2024-10-16 23:27:12 +0530

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+import Navbar from "@/components/dock";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Moharnab's Portfolio",
+  description: "Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.png" sizes="any" />
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar></Navbar>
+
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ data/index.ts
@@ -0,0 +1,221 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize quality over quantity",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Seemed Cool",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently aiming to learn FrontEnd",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Evade",
+    title2:"/evade.rar",
+    des: "A 2D platformer game made using Unreal Engine and a 2D sprite editor",
+    img: "/evadess.png",
+    iconLists: ["/UE.svg", "/C++.svg"],
+    link: "https://www.mediafire.com/file/esmz80s0b1tnzlz/Evade.rar/file",
+  },
+  {
+    id: 2,
+    title: "FPS Multiplayer Game",
+    title2:"/fps.rar",
+    des: "A FPS game made using Unity whose multiplayer runs on the Photon Engine.",
+    img: "/fps.png",
+    iconLists: ["/unity.svg", "/c--4.svg"],
+    link: "https://www.mediafire.com/file/wjh6kobirb0qoht/fps_multiplayer.rar/file",
+  },
+  {
+    id: 3,
+    title: "Metapparels",
+    title2: "/metapparels",
+    des: "A demo website for a clothwear brand for which helped us crack hackathon",
+    img: "/metaparels.png",
+    iconLists: ["/nextjs.svg", "/tail.svg", "/ts.svg","/js.svg"],
+    link: "https://github.com/EnderMRG/tryingwebsite",
+  },
+  {
+    id: 4,
+    title: "Portfolio",
+    title2: "/portfolio",
+    des: "A website to display my portfolio",
+    img: "/portfolio.png",
+    iconLists: ["/nextjs.svg", "/tail.svg", "/ts.svg","/js.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "UnrealEngine",
+    img: "/UE.svg",
+    nameImg: "",
+  },
+  {
+    id: 2,
+    name: "Unity",
+    img: "/unity.svg",
+    nameImg: "",
+  },
+  {
+    id: 3,
+    name: "C",
+    img: "/Clogo.svg",
+    nameImg: "",
+  },
+  {
+    id: 4,
+    name: "C++",
+    img: "/C++logo.svg",
+    nameImg: "",
+  },
+  {
+    id: 5,
+    name: "C.",
+    img: "/c--4.svg",
+    nameImg: "",
+  },
+  {
+    id: 6,
+    name: "html",
+    img: "/html.svg",
+    nameImg: "",
+  },
+  {
+    id: 7,
+    name: "css",
+    img: "/css.svg",
+    nameImg: "",
+  },
+  {
+    id: 8,
+    name: "nextjs",
+    img: "/nextjs.svg",
+    nameImg: "",
+  },
+   {
+    id: 9,
+    name: "tailwind",
+    img: "/tailwind.svg",
+    nameImg: "",
+  },
+  {
+    id: 10,
+    name: "java",
+    img: "/java.svg",
+    nameImg: "",
+  },
+  {
+    id: 11,
+    name: "javascript",
+    img: "/js.svg",
+    nameImg: "",
+  },
+  {
+    id: 12,
+    name: "trans",
+    img: "/ts.svg",
+    nameImg: "",
+  },
+  {
+    id: 13,
+    name: "python",
+    img: "/python.svg",
+    nameImg: "",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Tech-Team Member",
+    desc: "Currently working as a tech team member in GDG on Campus Dibrugarh",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Cracked Hackathon",
+    desc: "Cracked my first front-end hackathon with a group of 3 ",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
